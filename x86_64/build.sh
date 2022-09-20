b97bd98c49ebe44b8ea3c918f493622a0ed26177
Ole Jørgen Abusdal <ojab@158-37-70-239.hvl.no>
2022-09-20 16:05:24 +0200

    build for l64, sort out symbol names later

--- x86_64/build.sh
+++ x86_64/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Q1: Why can't we just do e.g "CONFIG=m1mac" and run this script?
-#     Why must we do "export config=m1mac" to run it as intended? 
-
-
 case $CONFIG in
 
 m1mac)
@@ -12,10 +8,10 @@ m1mac)
 	ld -L /Library/Developer/CommandLineTools/SDKs/MacOSX12.3.sdk/usr/lib -macosx_version_min 12.3.0 -lc -o bf bf.o parse.o
 	;;
 	
-linux32)
-	nasm -felf -Fdwarf -g bf.asm
-	gcc -c -m32 -Wall parse.c -g -o parse.o
-	gcc -m32 parse.o bf.o -o bf
+linux64)
+	nasm -felf64 -Fdwarf -g bf.asm
+	gcc -c -m64 -Wall parse.c -g -o parse.o
+	gcc -m64 parse.o bf.o -o bf
 	;;
 *)
 	printf "unknown configuration\n"
